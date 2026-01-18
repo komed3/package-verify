eb9901f4df749f8be7d4c19bd4a94c9df556cc7a
komed3 <pk97@outlook.de>
2026-01-18 11:21:30 +0100

    implement VerifyPkgManifest type based on schema

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,45 @@
+export type VerifyPkgPolicyLevel = 'error' | 'warn' | 'ignore';
+
 export interface VerifyPkgOptions {
     manifestPath: string;
     verbose?: boolean;
 }
 
-export interface VerifyPkgManifest {}
+export interface VerifyPkgManifest {
+    meta: {
+        manifestVersion: 1;
+    };
+    context: {
+        packageRoot: string;
+    };
+    policy: {
+        defaultSeverity: VerifyPkgPolicyLevel;
+        failOnWarnings?: boolean;
+        unexpectedFiles?: VerifyPkgPolicyLevel;
+        on?: {
+            missingExpected?: VerifyPkgPolicyLevel;
+            emptyPattern?: VerifyPkgPolicyLevel;
+            deriveFailure?: VerifyPkgPolicyLevel;
+        };
+    };
+    expect: {
+        files?: string[];
+        patterns?: string[];
+        atLeastOne?: string[][];
+    };
+    derive?: {
+        sources: {
+            root: string;
+            include: string;
+            exclude?: string[];
+        };
+        rules: Array< {
+            match?: string[];
+            default?: boolean;
+            mode: string;
+        } >;
+        targets: Record< string, string[] >;
+    };
+}
 
 export interface VerifyPkgResult {}
