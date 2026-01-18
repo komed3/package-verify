afacef0654d0e9854d7d9cf02807dfdae736c429
komed3 <pk97@outlook.de>
2026-01-18 13:00:51 +0100

    instantiate PackageVerifier cls

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { ManifestLoader } from './core/ManifestLoader';
 import { ManifestNormalizer } from './core/ManifestNormalizer';
+import { PackageVerifier } from './core/PackageVerifier';
 import { VerifyPkgOptions, VerifyPkgResult } from './types';
 
 export default async function verifyPackage ( opt: VerifyPkgOptions ) : Promise< VerifyPkgResult > {
     const { manifestPath, cwd, verbose = false } = opt;
+
     const manifest = await ManifestLoader.load( manifestPath );
     const normalized = ManifestNormalizer.normalize( manifest, cwd );
+
+    const verifier = new PackageVerifier ( normalized, verbose );
 
     return {};
 }
