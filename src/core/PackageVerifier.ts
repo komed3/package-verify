58fedef3e7675eab484a36e8545368914220e8ee
komed3 <pk97@outlook.de>
2026-01-18 13:55:31 +0100

    implement private helper methode glob()

--- src/core/PackageVerifier.ts
+++ src/core/PackageVerifier.ts
@@ -1,4 +1,6 @@
 import { VerifyPkgNormalized, VerifyPkgResult } from '../types';
+import { readdir } from 'node:fs/promises';
+import { join, relative } from 'node:path';
 
 export class PackageVerifier {
 
@@ -6,6 +8,22 @@ export class PackageVerifier {
         private readonly manifest: VerifyPkgNormalized,
         private readonly verbose: boolean
     ) {}
+
+    private async glob ( base: string, regex: RegExp ) : Promise< string[] > {
+        const files: string[] = [];
+
+        const traverse = async ( dir: string ) => {
+            for ( const e of await readdir( dir, { withFileTypes: true } ) ) {
+                const fullPath = join( dir, e.name );
+                e.isDirectory() ? await traverse( fullPath ) : files.push(
+                    relative( base, fullPath )
+                );
+            }
+        };
+
+        await traverse( base );
+        return files.filter( f => regex.test( f ) );
+    }
 
     public async verify () : Promise< VerifyPkgResult > {
         return {};
