6667949e213e9d152fbb6af3b324cfdb42f1d817
komed3 <pk97@outlook.de>
2026-01-18 10:24:55 +0100

    load manifest file

--- src/core/ManifestLoader.ts
+++ src/core/ManifestLoader.ts
@@ -1,9 +1,14 @@
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
 import Ajv from 'ajv';
 
 export class ManifestLoader {
 
     private readonly ajv = new Ajv( { allErrors: true, strict: true } );
 
-    public async load ( path: string ) : Promise< any > {}
+    public async load ( path: string ) : Promise< any > {
+        const raw = await readFile( join( process.cwd(), path ), 'utf8' );
+        const manifest = JSON.parse( raw );
+    }
 
 }
