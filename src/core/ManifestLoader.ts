073d0734d3737b8cfdc302a9bd5f9e5bdfa95145
komed3 <pk97@outlook.de>
2026-01-18 10:21:33 +0100

    ManifestLoader design

--- src/core/ManifestLoader.ts
+++ src/core/ManifestLoader.ts
@@ -1,1 +1,9 @@
-export class ManifestLoader {}
+import Ajv from 'ajv';
+
+export class ManifestLoader {
+
+    private readonly ajv = new Ajv( { allErrors: true, strict: true } );
+
+    public async load ( path: string ) : Promise< any > {}
+
+}
